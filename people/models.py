60dbab6c1ed7a557333f005a9ee6daadf383172d
Dayra Iñaguazo <dlinahuazo@tecnologicosudamericano.edu.ec>
2020-09-24 09:50:56 -0500

    Agregar modelos.

--- people/models.py
+++ people/models.py
@@ -1,3 +1,93 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class Persona(models.Model):
+    TIPO_DOCUMENTO = (
+        ('DNI', 'DNI'),
+        ('RUC', 'Ruc'),
+        ('PASAPORTE', 'Pasaporte'),
+    )
+    nombre = models.CharField(
+        max_length=250, verbose_name='Nombres')
+    apellido = models.CharField(
+        max_length=250, verbose_name='Apellidos')
+    numero_identificacion = models.CharField(
+        max_length=100, verbose_name='Número de Identificación')
+    tipo_documento_identificacion = models.CharField(
+        max_length=50,
+        choices=TIPO_DOCUMENTO,
+        default='CEDULA',
+        verbose_name='Tipo de Identificación'
+    )
+
+
+class Direccion(models.Model):
+    TIPO_DIRECCION = (
+        ('TRABAJO', 'Trabajo'),
+        ('CASA', 'Domicilio'),
+    )
+    calle_principal = models.CharField(
+        max_length=250, verbose_name='Calle Principal')
+    calle_secundaria = models.CharField(
+        max_length=250, null=True, blank=True, verbose_name='Calle Secundaria')
+    telefono = models.CharField(max_length=250, verbose_name='Teléfono')
+    referencia = models.TextField(verbose_name='Referencia', null=True, blank=True,)
+    tipo_direccion = models.CharField(
+        max_length=50,
+        choices=TIPO_DIRECCION,
+        default='CASA',
+        verbose_name='Tipo de Dirección'
+    )
+    persona = models.ForeignKey(
+        Persona, on_delete=models.CASCADE, related_name='direcciones')
+
+
+class ManejadorUsuarios(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('Usuario debe tener un correo valido.')
+
+        if not kwargs.get('username'):
+            raise ValueError('El usuario debe tener un nombre valido.')
+
+        usuario = self.model(correo_electronico=self.normalize_email(email),
+                             nombre_de_usuario=kwargs.get('username')
+                             )
+
+        usuario.set_password(password)
+        usuario.save()
+
+        return usuario
+
+    def create_superuser(self, email, password, **kwargs):
+        usuario = self.create_user(email, password, **kwargs)
+        usuario.is_admin = True
+        usuario.is_staff = True
+        usuario.is_superuser = True
+        usuario.save()
+        return usuario
+
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    correo_electronico = models.EmailField(
+        unique=True, blank=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=True)
+    nombre_de_usuario = models.CharField(max_length=50, unique=True)
+    persona = models.OneToOneField(
+        Persona, null=True, on_delete=models.CASCADE,)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'correo_electronico'
+    REQUIRED_FIELDS = [correo_electronico]
+
+    objects = ManejadorUsuarios()
+
+    class Meta:
+        ordering = ['persona', 'nombre_de_usuario']
+
+    def __str__(self):
+        return self.nombre_de_usuario
--- store/models.py
+++ store/models.py
@@ -1,3 +1,99 @@
 from django.db import models
+from people.models import Persona
 
-# Create your models here.
+
+class Empresa(models.Model):
+    nombre = models.CharField(
+        max_length=250, verbose_name='Nombre')
+    nombre_contacto = models.CharField(
+        max_length=250, verbose_name='Contacto')
+    email = models.EmailField(
+        unique=True, verbose_name='Correo electrónico')
+    telefono = models.CharField(
+        max_length=250, verbose_name='Teléfono', null=True, blank=True)
+    celular = models.CharField(
+        max_length=250, verbose_name='Celular', null=True, blank=True)
+    direccion = models.CharField(
+        max_length=250, verbose_name='Dirección', null=True, blank=True)
+
+
+class Proveedor(models.Model):
+    nombre = models.CharField(
+        max_length=250, verbose_name='Nombre')
+    email = models.EmailField(
+        unique=True, verbose_name='Correo electrónico')
+    telefono = models.CharField(
+        max_length=250, verbose_name='Teléfono', null=True, blank=True)
+    celular = models.CharField(
+        max_length=250, verbose_name='Celular', null=True, blank=True)
+    direccion = models.CharField(
+        max_length=250, verbose_name='Dirección', null=True, blank=True)
+
+
+class Tecnico(Persona):
+    fecha_ingreso = models.DateField(verbose_name="Fecha de ingreso")
+
+
+class Cliente(Persona):
+    def __str__(self):
+        return self.numero_identificacion
+
+
+class OrdenMantenimiento(models.Model):
+    ESTADO = (
+        ('NUEVO', 'Nuevo'),
+        ('EN_REVISION', 'EN revisión'),
+        ('REVISADO', 'Revisado'),
+        ('FINALIZADO', 'Finalizado'),
+    )
+    fecha_registro = models.DateField(
+        verbose_name="Fecha de registro", auto_now_add=True)
+    descripcion = models.CharField(
+        max_length=250, verbose_name='Descripción', null=True, blank=True)
+    estado = models.CharField(
+        max_length=50,
+        choices=ESTADO,
+        default='NUEVO',
+        verbose_name='Estado'
+    )
+    monto_servicio = models.DecimalField(
+        max_digits=12, decimal_places=2, verbose_name='Monto', default=0.00)
+    cliente = models.ForeignKey(
+        Cliente, on_delete=models.CASCADE, related_name='ordenes')
+
+
+class DetalleOrden(models.Model):
+    ESTADO = (
+        ('NUEVO', 'Nuevo'),
+        ('EN_REVISION', 'EN revisión'),
+        ('REVISADO', 'Revisado'),
+        ('CONFIRMADO', 'Confirmado'),
+        ('Arregado', 'Arreglado'),
+        ('FINALIZADO', 'Finalizado'),
+    )
+    nombre_equipo = models.CharField(
+        max_length=250, verbose_name='Nombre de equipo')
+    observacion = models.CharField(
+        max_length=250, verbose_name='Observación')
+
+    estado = models.CharField(
+        max_length=50,
+        choices=ESTADO,
+        default='NUEVO',
+        verbose_name='Estado'
+    )
+    precio_servicio = models.DecimalField(
+        max_digits=12, decimal_places=2, verbose_name='Precio de servicio', default=0.00)
+    orden_mantenimiento = models.ForeignKey(
+        OrdenMantenimiento, on_delete=models.CASCADE, related_name='detalles')
+
+
+class RevisionTecnica(models.Model):
+    fecha_revision = models.DateField(
+        verbose_name="Fecha de revisión")
+    descripcion = models.CharField(
+        max_length=250, verbose_name='Descripción')
+    tecnico = models.ForeignKey(
+        Tecnico, on_delete=models.CASCADE, related_name='revisiones')
+    detalle_orden = models.ForeignKey(
+        DetalleOrden, on_delete=models.CASCADE, related_name='revisiones')
