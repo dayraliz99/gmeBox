92644634a6dc864a8b40101c8eb1c70e2e1535f3
Dayra Iñaguazo <dlinahuazo@tecnologicosudamericano.edu.ec>
2020-10-04 17:37:27 -0500

    Agregando admin lte

--- store/forms.py
+++ store/forms.py
@@ -1,3 +1,9 @@
 from django import forms
 from store.models import Tecnico
 from django.forms import ModelForm
+
+
+class TecnicoForm(ModelForm):
+    class Meta:
+        model = Tecnico
+        fields = '__all__'
